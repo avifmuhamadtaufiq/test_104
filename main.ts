5d1036dde2ffdc6c0f808b6cc54d1340d6dd5512
avif muhamad taufiq <avifmuhamadtaufiq4@gmail.com>
2022-08-08 22:01:36 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,44 @@
 function init_esp8266 () {
     esp8266.init(SerialPin.P13, SerialPin.P14, BaudRate.BaudRate115200)
     if (esp8266.isESP8266Initialized()) {
-        OLED.writeStringNewLine("init esp8266 success")
-    } else {
-        OLED.writeStringNewLine("init esp8266 error")
+    	
     }
 }
+datalogger.onLogFull(function () {
+    logging = false
+    basic.showIcon(IconNames.Skull)
+})
+function init_dht22 () {
+    dht11_dht22.queryData(
+    DHTtype.DHT22,
+    DigitalPin.P0,
+    false,
+    false,
+    false
+    )
+}
+input.onButtonPressed(Button.A, function () {
+    logging = !(logging)
+    if (logging) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (input.logoIsPressed()) {
+        basic.showIcon(IconNames.No)
+        datalogger.deleteLog(datalogger.DeleteType.Full)
+        logging = false
+        datalogger.setColumnTitles(
+        "time",
+        "temprature"
+        )
+    }
+})
 function init_ds3231 () {
     esp8266.updateInternetTime()
     if (esp8266.isInternetTimeUpdated()) {
-        OLED.writeStringNewLine("init ds3231")
         DS3231.dateTime(
         esp8266.getYear(),
         esp8266.getMonth(),
@@ -19,47 +48,42 @@ function init_ds3231 () {
         esp8266.getMinute(),
         esp8266.getSecond()
         )
-    } else {
-        OLED.writeStringNewLine("error time")
+        led.plot(3, 0)
     }
-    basic.pause(1000)
-}
-function init_oled () {
-    OLED.init(128, 64)
 }
 function init_internet_time () {
     esp8266.initInternetTime(7)
     if (esp8266.isInternetTimeInitialized()) {
-        OLED.writeStringNewLine("init time success")
-    } else {
-        OLED.writeStringNewLine("init time error")
+        led.plot(2, 0)
     }
 }
 function init_wifi () {
     esp8266.connectWiFi("OFF", "wordwide444")
     if (esp8266.isWifiConnected()) {
-        OLED.writeStringNewLine("wifi connected")
-    } else {
-        OLED.writeStringNewLine("wifi disconnected")
+        led.plot(1, 0)
     }
 }
-init_oled()
-basic.pause(2000)
-OLED.clear()
+let temprature = 0
+let logging = false
+logging = false
+datalogger.setColumnTitles(
+"time",
+"temprature"
+)
 init_esp8266()
-basic.pause(2000)
-OLED.clear()
 init_wifi()
-basic.pause(2000)
-OLED.clear()
 init_internet_time()
-basic.pause(2000)
-OLED.clear()
 init_ds3231()
-basic.pause(2000)
-OLED.clear()
-basic.forever(function () {
-    OLED.clear()
-    OLED.writeStringNewLine("" + DS3231.hour() + ":" + DS3231.minute() + ":" + DS3231.second())
-    basic.pause(1000)
+basic.pause(5000)
+loops.everyInterval(1000, function () {
+    if (logging) {
+        init_dht22()
+        if (dht11_dht22.readDataSuccessful()) {
+            temprature = dht11_dht22.readData(dataType.temperature)
+        }
+        datalogger.log(
+        datalogger.createCV("time", "" + DS3231.hour() + ":" + DS3231.minute() + ":" + DS3231.second()),
+        datalogger.createCV("temprature", temprature)
+        )
+    }
 })
